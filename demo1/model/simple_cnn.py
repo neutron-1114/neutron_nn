dedccf025ae727d32dfa1110bfcc1f1344d20e8a
tongzi <tongzi@ibantang.com>
2023-08-24 18:01:13 +0800

    INIT

--- demo1/model/simple_cnn.py
+++ demo1/model/simple_cnn.py
@@ -40,7 +40,7 @@ class Cifar10DoubleCNN(nn.Module):
     def __init__(self):
         super(Cifar10DoubleCNN, self).__init__()
         self.layers = nn.Sequential(
-            nn.Conv2d(in_channels=1, out_channels=16, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2),
             nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, stride=1, padding=1),
