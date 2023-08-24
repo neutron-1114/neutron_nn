b6da19b345a82ba0d3cbfc1e88eb76bcf7daee84
tongzi <tongzi@ibantang.com>
2023-08-24 18:00:36 +0800

    INIT

--- demo1/model/simple_cnn.py
+++ demo1/model/simple_cnn.py
@@ -39,21 +39,19 @@ class Cifar10SimpleCNN(nn.Module):
 class Cifar10DoubleCNN(nn.Module):
     def __init__(self):
         super(Cifar10DoubleCNN, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3, stride=1, padding=1)
-        self.relu1 = nn.ReLU()
-        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.conv2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, stride=1, padding=1)
-        self.relu2 = nn.ReLU()
-        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.fc = nn.Linear(32 * 8 * 8, 10)  # Assuming input image size is 32x32
+        self.layers = nn.Sequential(
+            nn.Conv2d(in_channels=1, out_channels=16, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Flatten(),
+            nn.Linear(64 * 4 * 4, 10)
+        )
 
     def forward(self, x):
-        x = self.conv1(x)
-        x = self.relu1(x)
-        x = self.pool1(x)
-        x = self.conv2(x)
-        x = self.relu2(x)
-        x = self.pool2(x)
-        x = x.view(x.size(0), -1)  # Flatten the tensor
-        x = self.fc(x)
-        return x
+        return self.layers.forward(x)
